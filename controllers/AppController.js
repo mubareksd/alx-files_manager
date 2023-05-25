25a5414b37fff9c97e4c53b1333ea78229c6eadc
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-25 19:15:34 +0300

    task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
+
+class AppController {
+  static getStatus = (req, res) => {
+    return res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  };
+
+  static getStats = async (req, res) => {
+    return res.status(200).json({
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    });
+  };
+}
+
+export default AppController;
---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import router from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/", router);
+
+const PORT = parseInt(process.env.PORT) || 5000;
+
+app.listen(PORT, () => {
+  console.log(`connected on http://localhost:${PORT}`);
+});
+
+export default app;
