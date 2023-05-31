daa55792d922d7f45062d0d2015ddba3688fa97e
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-05-31 17:50:29 +0300

    revert

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,5 @@
 import AppController from './AppController';
 import AuthController from './AuthController';
-import FilesController from './FilesController';
 import UsersController from './UsersController';
 
-export {
-  AppController,
-  AuthController,
-  FilesController,
-  UsersController,
-};
+export { AppController, AuthController, UsersController };
